7df0bcc95624a6aeaf5a31e7d5412b3414ad676e
Srishti Gupta <srishti_gupta@mcafee.com>
2025-03-14 23:11:20 +0530

    Added docker compose ans Auth init

---
+++ services/auth-service/config/database.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+// Global DB
+var DB *pgx.Conn
+
+func ConnectDataBase() {
+	_ = godotenv.Load()
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := pgx.Connect(ctx, dsn)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+
+	log.Println("Connected to PostgreSQL!")
+	DB = conn
+}
---
+++ services/auth-service/go.mod
@@ -0,0 +1,17 @@
+module github.com/srishti13722/ai-hirehub/auth-service
+
+go 1.23.0
+
+toolchain go1.23.7
+
+require (
+	github.com/jackc/pgx/v5 v5.7.2
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+)
