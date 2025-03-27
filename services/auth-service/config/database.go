1ce32452879dee44e9e6c30ff2d911f5676b9326
Srishti Gupta <srishti_gupta@mcafee.com>
2025-03-27 15:00:48 +0530

    added migrations

--- services/auth-service/config/database.go
+++ services/auth-service/config/database.go
@@ -36,3 +36,23 @@ func ConnectDataBase() {
 	log.Println("Connected to PostgreSQL!")
 	DB = conn
 }
+
+func RunMigrations() {
+	query := `
+	CREATE TABLE IF NOT EXISTS users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    firstname VARCHAR(20) NOT NULL,
+    lastname VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(20) CHECK (role in('job_seeker', 'recruiter', 'admin')) NOT NULL DEFAULT 'job_seeker',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);`
+
+	_, err := DB.Exec(context.Background(), query)
+	if err != nil {
+		log.Fatal("Failed to run migrations:", err)
+	}
+
+	log.Println("Users table checked/created.")
+}
